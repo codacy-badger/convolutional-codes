766f88ea09352caca6e038e9b89e5915b93f2c0d
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-06-25 00:36:24 -0400

    Update convolutional.rb

--- src/convolutional.rb
+++ src/convolutional.rb
@@ -2,29 +2,29 @@
 
 # Generator polynomials will be represented as a hash of lists,
 # e.g. G0 = 111 and G1 = 110 would equal
-polynomials = {
-  'G0' => [1, 1, 1],
-  'G1' => [1, 1, 0]
-}
+# polynomials = {
+#   'G0' => [1, 1, 1],
+#   'G1' => [1, 1, 0]
+# }
 # Finite-state machines will be represented similarly.
 # Take, for example, a convolutional code with constraint length 3 and the aforementioned polynomials.
-ex_fsa = {
-  '00' => [
-    { 'new' => '00', 'out' => '00' },
-    { 'new' => '10', 'out' => '11' }
-  ],
-  '01' => [
-    { 'new' => '00', 'out' => '10' },
-    { 'new' => '10', 'out' => '01' }
-  ],
-  '10' => [
-    { 'new' => '01', 'out' => '11' },
-    { 'new' => '11', 'out' => '00' }
-  ],
-  '11' => [
-    { 'new' => '01', 'out' => '01' },
-    { 'new' => '11', 'out' => '10' }
-  ]
+# ex_fsa = {
+#   '00' => [
+#     { 'new' => '00', 'out' => '00' },
+#     { 'new' => '10', 'out' => '11' }
+#   ],
+#   '01' => [
+#     { 'new' => '00', 'out' => '10' },
+#     { 'new' => '10', 'out' => '01' }
+#   ],
+#   '10' => [
+#     { 'new' => '01', 'out' => '11' },
+#     { 'new' => '11', 'out' => '00' }
+#   ],
+#   '11' => [
+#     { 'new' => '01', 'out' => '01' },
+#     { 'new' => '11', 'out' => '10' }
+#   ]
 }
 # Each key is a state, and each value is a list of hashes
 # representing the new state and output when given an input (the index).
